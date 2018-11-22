1d26a65e513180ec2aea184c3e51b8afdc8d427b
Manuel Pernold <6154@htl.rennweg.at>
2018-11-22 12:49:31 +0100

    Main coded

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(80)) {
+            while (true) {
+                Socket client = serverSocket.accept();
+                ServerThread thread = new ServerThread(client);
+                thread.run();
+            }
+        } catch (IOException e) {
+
+        }
+    }
 }
