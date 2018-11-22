4e1e5392aafaa334b5eb5cd7f0c4d00d55dd8f05
Manuel Pernold <6154@htl.rennweg.at>
2018-11-22 09:55:40 +0100

    ServerThread added, run() coded

---
+++ src/ServerThread.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread extends Thread{
+    Socket client;
+
+    public ServerThread(Socket client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(client.getInputStream()));
+             BufferedWriter w = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()))) {
+            w.write("hello Sir!");
+            w.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
